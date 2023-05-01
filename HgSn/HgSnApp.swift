520801723cbae83731cab5335a21447995d979a7
박준현 <108358152+Wd2wad3@users.noreply.github.com>
2023-05-01 10:32:35 +0900

    Initial Commit

---
+++ HgSn/HgSnApp.swift
@@ -0,0 +1,17 @@
+//
+//  HgSnApp.swift
+//  HgSn
+//
+//  Created by 4rNe5 on 2023/05/01.
+//
+
+import SwiftUI
+
+@main
+struct HgSnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
