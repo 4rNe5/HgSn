2cb1572e84e247ef1477bb53925b496a28ee4c69
박준현 <108358152+Wd2wad3@users.noreply.github.com>
2023-05-01 10:46:48 +0900

    first commit

--- HgSn/ContentView.swift
+++ HgSn/ContentView.swift
@@ -4,18 +4,59 @@
 //
 //  Created by 4rNe5 on 2023/05/01.
 //
-
 import SwiftUI
+import Alamofire
 
 struct ContentView: View {
+    
+    @State var temp: RiverTemperature?
+    
+    func sendAPIRequest() {
+        let url = "http://openapi.seoul.go.kr:8088/6e416945703233773131316f716e6d53/json/WPOSInformationTime/1/5"
+        
+        AF.request(url,
+                   method: .get,
+                   parameters: nil,
+                   encoding: JSONEncoding(options: []),
+                   headers: ["Content-Type":"application/json", "Accept":"application/json"])
+        .validate(statusCode: 200..<300)
+        .responseJSON { response in
+            switch response.result {
+            case .success(let data):
+                do {
+                    // 응답 데이터를 JSON 데이터로 변환하지 않고, response.data를 그대로 사용하여 디코딩
+                    self.temp = try JSONDecoder().decode(RiverTemperature.self, from: response.data!)
+                } catch {
+                    print(error)
+                }
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "humidity")
+                .resizable()
+                .frame(width: 200, height: 150)
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, Hangang!")
+                .font(.system(size: 25))
+                .bold()
+                .foregroundColor(.blue)
+                .padding()
+            Text("지금 한강은 \(temp?.wposInformationTime.row.first?.wTemp ?? "")도 입니다!")
+                .font(.system(size: 18))
+                .bold()
+                .foregroundColor(.blue)
         }
         .padding()
+        .onAppear {
+            sendAPIRequest()
+        }
     }
 }
 
@@ -24,3 +65,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
---
+++ HgSn/Rvtp.swift
@@ -0,0 +1,45 @@
+//
+//  Rvtp.swift
+//  HgSn
+//
+//  Created by 4rNe5 on 2023/05/01.
+//
+struct RiverTemperature: Codable {
+    let listTotalCount: Int
+    let result: Result
+    let wposInformationTime: WposInformationTime
+
+    enum CodingKeys: String, CodingKey {
+        case listTotalCount = "list_total_count"
+        case result
+        case wposInformationTime = "WPOSInformationTime"
+    }
+}
+
+struct Result: Codable {
+    let code, message: String
+}
+
+struct WposInformationTime: Codable {
+    let row: [Row]
+}
+
+struct Row: Codable {
+    let msrDate, msrTime, siteID, wTemp: String
+    let wPH, wDO, wTN, wTP: String
+    let wTOC, wPHEN, wCN: String
+
+    enum CodingKeys: String, CodingKey {
+        case msrDate = "MSR_DATE"
+        case msrTime = "MSR_TIME"
+        case siteID = "SITE_ID"
+        case wTemp = "W_TEMP"
+        case wPH = "W_PH"
+        case wDO = "W_DO"
+        case wTN = "W_TN"
+        case wTP = "W_TP"
+        case wTOC = "W_TOC"
+        case wPHEN = "W_PHEN"
+        case wCN = "W_CN"
+    }
+}
